cba96debd8df16b5454c5a46c51971238af477b6
Brad Mortensen <b.mort86@gmail.com>
2019-07-06 22:23:32 -0600

    adds skill group file

---
+++ src/components/SkillGroup.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const StyledSkillGroup = styled.div`
+  width: 47%;
+  box-sizing: border-box;
+  position: relative;
+  margin-bottom: 30px;
+  @media (max-width: 900px) {
+    width: 96%;
+  }
+`;
+
+const SkillGroup = () => {
+  return (
+    <StyledSkillGroup>
+      
+    </StyledSkillGroup>
+  )
+}
+
+export default SkillGroup
