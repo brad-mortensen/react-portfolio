456c0df084177e5b6e7bc0ac27c4040d1ca0693b
Brad Mortensen <b.mort86@gmail.com>
2019-07-07 21:30:44 -0600

    adds remaining vector graphics

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import javascript from './assets/javascript';
+import nodejs from './assets/nodejs-1';
+import npm from './assets/npm';
+import postgresql from './assets/postgresql';
+import vue from './assets/vue-9';
+import react from './assets/react';
+import python from './assets/python-5';
+import yarn from './assets/yarn';
+import sqlite from './assets/sqlite';
+import c from './assets/c';
+import django from './assets/django';
+
+
 import styled from 'styled-components';
 import SkillGroup from './SkillGroup';
+
+
 
 const StyledSkills = styled.div`
   box-sizing: border-box;
@@ -29,29 +44,87 @@ const StyledSkills = styled.div`
 const Skills = () => {
   const skillData = [
     {
-      groupName: "javascript",
-      assets: [],
+      groupName: "languages",
+      assets: [
+        {
+          name: 'javascript',
+          img: javascript,
+          id: 0
+        },
+        {
+          name: 'python',
+          img: python,
+          id: 1
+        },
+        {
+          name: 'c',
+          img: c,
+          id: 2
+        }
+      ],
       id: 0
     },
     {
-      groupName: "python",
-      assets: [],
+      groupName: "frontend",
+      assets: [
+        {
+          name:'react',
+          img: react,
+          id:0
+        },
+        {
+          name:'vue',
+          img: vue,
+          id:1
+        },
+        {
+          name:'django',
+          img: django,
+          id:2
+        }
+      ],
       id: 1
     },
     {
       groupName: "backend",
       assets: [
-        // {
-        //   name:
-        //   img,
-        //   id:0
-        // }
+        {
+          name: 'nodejs',
+          img: nodejs,
+          id: 0
+        },
+        {
+          name: 'postgresql',
+          img: postgresql,
+          id: 1
+        },
+        {
+          name: 'sqlite',
+          img: sqlite,
+          id: 2
+        }
       ],
       id: 2
     },
     {
       groupName: "tools",
-      assets: [],
+      assets: [
+        {
+          name:'git',
+          img: git,
+          id:0
+        },
+        {
+          name:'npm',
+          img: npm,
+          id:1
+        },
+        {
+          name:'yarn',
+          img:yarn,
+          id:2
+        }
+      ],
       id: 3
     },
   ]
