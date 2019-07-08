db2a39f15ee59442f561b88cc5c56be6506d7446
Brad Mortensen <b.mort86@gmail.com>
2019-07-08 13:52:33 -0600

    more break points

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,30 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledNav = styled.div`
+margin-top: 10px;
   width: 100%;
-  height: 120px;
+  height: auto;
   display: flex;
   flex-flow: row;
   justify-content:space-between;
   align-items: center;
   box-sizing:border-box;
   padding: 0 3%;
-  .main-header {
+  @media(max-width: 700px){
+    flex-flow:column-reverse;
+  }
+  .main-header {   
     font-size: 1rem;
     text-transform: uppercase;
     font-family: 'Carrois Gothic SC', sans-serif;
+
   }
   .nav-links {
     display: flex;
     width: 30%;
     height: auto;
-    justify-content: space-between;    
+    justify-content: space-between; 
+    @media(max-width: 700px) {
+      width: 70%;
+      }
     p {
       cursor: pointer;
       height: 20px;
       text-transform: uppercase;
       font-size: 1rem;
       font-family: 'Carrois Gothic SC', sans-serif;
+      @media(max-width: 700px){
+        font-size: 1.2rem;
+      }
       &:hover{
         box-sizing: border-box;
         border-bottom: 1px solid black;
