5de6b29e9bf9ad0d87256d107d240c0ab776ca79
Brad Mortensen <b.mort86@gmail.com>
2019-10-07 12:25:13 -0600

    Reformats projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,38 +2,8 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 import { projectData } from './assets/data'
-
 import Project from './Project';
 
-const StyledProjects = styled.div`
-  box-sizing: border-box;
-  width: 100%;
-  height: auto;
-  display: flex;
-  justify-content: space-around;
-  align-items: top;
-  flex-flow: row wrap;
-  padding: 0 5% 30px 5%;
-  background-color: black;
-  @media (max-width: 900px) {
-    padding: 0 2%;
-  }
-  @media (max-width: 500px) {
-    flex-flow: column;
-    align-items: center;
-  }
-  .projects-header {
-    width: 100%;
-    font-family: 'Carrois Gothic SC', sans-serif;
-    font-size: 2rem;
-    font-weight: lighter;
-    color: white;
-    margin-bottom: 30px;
-    .brackets {
-      font-size: 1rem;
-    }
-  }
-`;
 
 const Projects = () => {
   const [isMobile, setIsMobile] = useState(false)
@@ -66,3 +36,33 @@ const Projects = () => {
 }
 
 export default Projects;
+
+const StyledProjects = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  height: auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: top;
+  flex-flow: row wrap;
+  padding: 0 5% 30px 5%;
+  background-color: black;
+  @media (max-width: 900px) {
+    padding: 0 2%;
+  }
+  @media (max-width: 500px) {
+    flex-flow: column;
+    align-items: center;
+  }
+  .projects-header {
+    width: 100%;
+    font-family: 'Carrois Gothic SC', sans-serif;
+    font-size: 2rem;
+    font-weight: lighter;
+    color: white;
+    margin-bottom: 30px;
+    .brackets {
+      font-size: 1rem;
+    }
+  }
+`;
