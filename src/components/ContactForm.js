f91ca7dfef7e8bc92c520015ebb742eed7c6ca32
Brad Mortensen <b.mort86@gmail.com>
2019-10-07 12:25:53 -0600

    reformats ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,66 +2,6 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import axios from 'axios';
 
-const Contact = styled.form`
-	width: 50%;
-	display: flex;
-	flex-flow: column;
-	align-items: center;
-	font-family: 'Carrois Gothic SC', sans-serif;
-	@media(max-width:835px) {
-		width: 100%;
-		align-items: flex-start;
-	}
-	label{
-		width: 60%;
-		color: white;
-		font-size: 1.2rem;
-		margin-bottom: 15px;
-		@media(max-width:835px) {
-			width: 90%;
-		}
-	}
-	input {
-		border: none;
-		width: 60%;
-		background-color: white;
-		margin-bottom: 20px;
-		height: 35px;
-		font-family: 'Carrois Gothic SC', sans-serif;
-		font-size: 1.2rem;
-		@media(max-width:835px) {
-			width: 90%;
-		}
-	}
-	textarea {
-		border: none;
-		box-sizing: border-box;
-		width: 60%;
-		height: 100px;
-		margin-bottom: 40px;
-		padding: 10px;
-		resize: none;
-		font-family: 'Carrois Gothic SC', sans-serif;
-		font-size: 1.2rem;
-		@media(max-width:835px) {
-			width: 90%;
-		}
-	}
-	.button--container {
-		width: 60%;
-		display: flex;
-		justify-content: flex-start;
-		button {
-			cursor: pointer;
-			width: 75%;
-			height: 50px;
-			background-color: white;
-			font-family: 'Carrois Gothic SC', sans-serif;
-			font-size: 1.5rem;
-			text-transform: uppercase;
-		}
-	}
-`;
 
 const ContactForm = () => {
 
@@ -134,3 +74,65 @@ const ContactForm = () => {
 };
 
 export default ContactForm;
+
+
+const Contact = styled.form`
+	width: 50%;
+	display: flex;
+	flex-flow: column;
+	align-items: center;
+	font-family: 'Carrois Gothic SC', sans-serif;
+	@media(max-width:835px) {
+		width: 100%;
+		align-items: flex-start;
+	}
+	label{
+		width: 60%;
+		color: white;
+		font-size: 1.2rem;
+		margin-bottom: 15px;
+		@media(max-width:835px) {
+			width: 90%;
+		}
+	}
+	input {
+		border: none;
+		width: 60%;
+		background-color: white;
+		margin-bottom: 20px;
+		height: 35px;
+		font-family: 'Carrois Gothic SC', sans-serif;
+		font-size: 1.2rem;
+		@media(max-width:835px) {
+			width: 90%;
+		}
+	}
+	textarea {
+		border: none;
+		box-sizing: border-box;
+		width: 60%;
+		height: 100px;
+		margin-bottom: 40px;
+		padding: 10px;
+		resize: none;
+		font-family: 'Carrois Gothic SC', sans-serif;
+		font-size: 1.2rem;
+		@media(max-width:835px) {
+			width: 90%;
+		}
+	}
+	.button--container {
+		width: 60%;
+		display: flex;
+		justify-content: flex-start;
+		button {
+			cursor: pointer;
+			width: 75%;
+			height: 50px;
+			background-color: white;
+			font-family: 'Carrois Gothic SC', sans-serif;
+			font-size: 1.5rem;
+			text-transform: uppercase;
+		}
+	}
+`;
