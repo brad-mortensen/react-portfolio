627dfa06aaa9a80de17254a69fbefe5baf92c836
Brad Mortensen <b.mort86@gmail.com>
2020-01-11 15:36:55 -0600

    realigns Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,44 +1,39 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import ContactForm from './ContactForm';
-import Socials from './Socials';
-
+import ContactForm from "./ContactForm";
+import Socials from "./Socials";
 
 const Footer = () => {
-
-	return (
-		<StyledFooter id="contact">
-			<p>Get in Touch</p>
-			<ContactForm />
-			<Socials />
-		</StyledFooter>
-	);
+  return (
+    <StyledFooter id="contact">
+      <p>Get in Touch</p>
+      <ContactForm />
+      <Socials />
+    </StyledFooter>
+  );
 };
 
 export default Footer;
 
-
-
 const StyledFooter = styled.div`
-	display:flex;
-	flex-flow: row wrap;
-	justify-content: space-around;
-	height: auto;
-	padding: 20px 5% 50px 5%;
-	background-color: black;
-	box-sizing: border-box;
-	@media(max-width: 700px) {
-		flex-flow: column;
-		
-	}
-	p {
-		width: 100%;
-		color: white;
-		font-size: 2rem;
-		font-family: 'Carrois Gothic SC', sans-serif;
-		font-weight: lighter;
-		text-transform: uppercase;
-		margin-bottom: 60px;
-	}
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: space-around;
+  height: auto;
+  padding: 20px 5% 50px 5%;
+  background-color: black;
+  box-sizing: border-box;
+  @media (max-width: 700px) {
+    flex-flow: column;
+  }
+  p {
+    width: 100%;
+    color: white;
+    font-size: 2rem;
+    font-family: "Carrois Gothic SC", sans-serif;
+    font-weight: lighter;
+    text-transform: uppercase;
+    margin-bottom: 60px;
+  }
 `;
